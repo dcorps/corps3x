d15891363cc57a2c161ebbb7944266c4c22d0ad1
dcorps <77898319+dcorps@users.noreply.github.com>
2021-01-30 23:43:47 +0300

    123

---
+++ precache-manifest.46ebed866857010892b595c26613dff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab5dfdc8480d69b5518ced5ce72b723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3071ec281e9edb02ec1",
+    "url": "/static/css/main.a817de01.chunk.css"
+  },
+  {
+    "revision": "769590ec81dac47758c6",
+    "url": "/static/js/2.6283a7a2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6283a7a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3071ec281e9edb02ec1",
+    "url": "/static/js/main.1b578117.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "2f86e04e9dc2754d8952c91a45939ca0",
+    "url": "/static/media/me.2f86e04e.png"
+  }
+]);
